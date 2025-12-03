362806c2c59b3cb096b50ee2df72c8009638649a
Pedro Lucas <lucaspires2322@gmail.com>
2025-12-02 23:14:52 -0300

    :sparkles: FEAT: Adicionando temas

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,36 @@
+export const theme = {
+    white: {
+        default: "#FFFFFF",
+        color10: "#fffffc",
+    },
+    whiteInput: "#F5F5F7",
+    placeholder: "#899796",
+    black: "#000000",
+
+    background: "#f5efff",
+
+    blue: {
+        color10: "#c1f1fc",
+        color20: "#a9dced",
+        color30: "#8fbfd6",
+        color40: "#",
+        color50: "#",
+        color60: "#",
+        color70: "#",
+        color80: "#0d2845",
+        color90: "#07172b",
+        color100: "#020914"
+    },
+
+    gray:{
+        gray10: "#f8f9fa",
+        gray20: "#e9ecef",
+        gray30: "#dee2e6",
+        gray40: "#ced4da",
+        gray50: "#adb5bd",
+        gray60: "#6c757d",
+        gray70: "#495057",
+        gray80: "#343a40",
+        gray90: "#212529",
+    }
+}
